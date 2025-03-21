aae7c91e283c1507752abd57e24553645c06a45c
TeunHoven <teun.d.hoven@gmail.com>
2025-03-21 11:03:39 +0100

    added project ids

--- lab3/pubsubclients/consumer.py
+++ lab3/pubsubclients/consumer.py
@@ -42,5 +42,5 @@ def callback(message):
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
-    create_subscription("your project id", "diabetes_res", "diabetes_res_sub")
-    pull_message(project="your project id", subscription="diabetes_res_sub")  # replace your project id
+    create_subscription("ada2025-450209", "diabetes_res", "diabetes_res_sub")
+    pull_message(project="ada2025-450209", subscription="diabetes_res_sub")  # replace your project id
